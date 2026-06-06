776926f67124cd8aaaf53a14cc600bc7bcc2d1a7
iteramental <jorge.morales@iteramental.org>
2026-06-06 08:59:29 -0500

    fix: handle standalone /uploads/ paths in absUrls

--- backend/src/controllers/publicController.js
+++ backend/src/controllers/publicController.js
@@ -4,7 +4,12 @@ const ApiResponse = require('../utils/apiResponse');
 const asyncHandler = require('../utils/asyncHandler');
 
 const absUrls = (obj, base) => {
-  if (typeof obj === 'string') return obj.replace(/(src=")(\/uploads\/)/g, `$1${base}$2`).replace(/(url\()(\/uploads\/)/g, `$1${base}$2`);
+  if (typeof obj === 'string') {
+    return obj
+      .replace(/(src=")(\/uploads\/)/g, `$1${base}$2`)
+      .replace(/(url\()(\/uploads\/)/g, `$1${base}$2`)
+      .replace(/^(\/uploads\/)/, `${base}$1`);
+  }
   if (Array.isArray(obj)) return obj.map(v => absUrls(v, base));
   if (obj && typeof obj === 'object') {
     for (const k of Object.keys(obj)) obj[k] = absUrls(obj[k], base);
